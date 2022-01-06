4ef78305f8a79d5a912300d6b589f5e39ff35716
Jovanni Lo <lodev09@gmail.com>
2022-01-07 01:33:54 +0800

    qualifications category

--- src/Options.php
+++ src/Options.php
@@ -4,13 +4,18 @@ namespace EZKnock;
 
 class Options extends Resource {
 
+    const QUALIFICATION_CATEGORY_STANDARD = 'standard';
+    const QUALIFICATION_CATEGORY_COVERAGE = 'coverage';
+
     /**
      * Get list of qualifications
-     * @see https://developers.ezknockmarketplace.com/reference#qualifications
+     * @see https://developers.ezknockmarketplace.com/reference#get-qualifications
      *
      * @return array
      */
-    public function getQualifications() {
-        return $this->client->get('/options/qualifications');
+    public function getQualifications($category = self::QUALIFICATION_CATEGORY_STANDARD) {
+        return $this->client->get('/options/qualifications', [
+            'category' => $category
+        ]);
     }
 }
